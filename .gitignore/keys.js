32e3874dac1727356aff00bc311591726b78ad8a
David Prieto <david.s.prieto1@gmail.com>
2023-04-30 18:57:22 -0500

    slight refactor to retrieve API key from keys.js file

--- .gitignore/keys.js
+++ .gitignore/keys.js
@@ -1,2 +1,2 @@
-// const mapBoxKey = 'pk.eyJ1IjoiZGF2aWRzcHJpZXRvIiwiYSI6ImNsMnFsZGtrdjAyZ28zYm9lYzNvOHVrbWYifQ.MInwhhSP2EcHcCPHsc5xYg';
-// const openWeatherMapApiKey = "3589206b4c332e7a308a23b883754111";
+const MAP_BOX_KEY = 'pk.eyJ1IjoiZGF2aWRzcHJpZXRvIiwiYSI6ImNsMnFsZGtrdjAyZ28zYm9lYzNvOHVrbWYifQ.MInwhhSP2EcHcCPHsc5xYg';
+const OPEN_WEATHER_KEY = "3589206b4c332e7a308a23b883754111";
